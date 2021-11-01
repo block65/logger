0c9aa0d3f8ea87be65282bfca3f2c2117f0faa5e
Max Holman <max@holmn.com>
2021-11-01 18:42:39 +0800

    fix: ts + esm testing with jest is a nightmare

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,12 @@
 module.exports = {
-  preset: 'ts-jest',
-  testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
+  preset: 'ts-jest/presets/default-esm',
   setupFiles: ['./jest-setup.cjs'],
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/dist/'],
   moduleNameMapper: {
     '^(\\..*)\\.jsx?$': '$1',
   },
+  testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
+  verbose: true,
+  extensionsToTreatAsEsm: ['.ts'],
 };
