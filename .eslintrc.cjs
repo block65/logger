0bb4d7253a597b1f9f371dcdfd4e0ba930e3e2a2
Max Holman <max@holmn.com>
2021-11-06 21:20:53 +0800

    chore: allow use of dev deps in jest files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,17 @@
 module.exports = {
   extends: '@block65',
   parserOptions: {
-    project: ["./tsconfig.json"]
-  }
+    project: ['./tsconfig.json'],
+  },
+  overrides: [
+    {
+      files: ['jest*.*js'],
+      rules: {
+        'import/no-extraneous-dependencies': [
+          'error',
+          { devDependencies: true },
+        ],
+      },
+    },
+  ],
 };
