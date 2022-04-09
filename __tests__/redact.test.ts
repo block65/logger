3ddb56a8382891694b68f0e657bcfd939aab6956
Max Holman <max@holmn.com>
2022-04-09 17:43:56 +0800

    fix: prevent crash when redacting undefined data + add tests

---
+++ __tests__/redact.test.ts
@@ -0,0 +1,39 @@
+import {
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  jest,
+  test,
+} from '@jest/globals';
+import { createRedactProcessor } from '../lib/index.js';
+import { createLoggerWithWaitableMock } from './helpers.js';
+
+describe('Redact', () => {
+  beforeEach(() => {
+    jest.useFakeTimers('modern');
+    jest.setSystemTime(new Date('2009-02-13T23:31:30.000Z'));
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  test('Basic', async () => {
+    const [logger, callback] = createLoggerWithWaitableMock({
+      processors: [
+        createRedactProcessor({
+          censor: '🧱🧱🧱🧱',
+          paths: ['secret'],
+        }),
+      ],
+    });
+
+    logger.error({ secret: 'trustno1' });
+    logger.error('trustno1');
+
+    await logger.flush();
+
+    await expect(callback.waitUntilCalledTimes(1)).resolves.toMatchSnapshot();
+  });
+});
