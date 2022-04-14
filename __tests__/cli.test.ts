1230ed2b699ac94993ba6ec113ce6922775983c1
Max Holman <max@holmn.com>
2022-04-14 21:04:44 +0800

    feat: improve color detection

--- __tests__/cli.test.ts
+++ __tests__/cli.test.ts
@@ -8,54 +8,71 @@ import {
 } from '@jest/globals';
 
 describe('CLI', () => {
+  const initialEnv = process.env;
   beforeEach(() => {
+    // jest.clearAllMocks();
     jest.useFakeTimers('modern');
     jest.setSystemTime(new Date('2009-02-13T23:31:30.000Z'));
   });
 
   afterEach(() => {
+    process.env = { ...initialEnv };
     jest.useRealTimers();
   });
 
-  test('no-color', async () => {
-    process.env.NO_COLOR = 'true';
-
-    const { createLoggerWithWaitableMock } = await import('./helpers.js');
+  test('force-color on', async () => {
+    const { Level, Logger } = await import('../lib/logger.js');
     const { createCliTransformer } = await import('../lib/transformers/cli.js');
 
-    const transformer = createCliTransformer();
-
-    const [logger, callback, errback] = createLoggerWithWaitableMock({
-      transformer,
-    });
-    logger.error(new Error('hallo'));
-    logger.info(new Error('hello'));
-    logger.warn(new Error('halo'));
-    logger.debug(new Error('gday'));
-    logger.trace(new Error('nihao2'));
-    await expect(callback.waitUntilCalledTimes(5)).resolves.toMatchSnapshot();
-    expect(errback).not.toBeCalled();
-  });
-
-  test('force-color', async () => {
-    const { createLoggerWithWaitableMock } = await import('./helpers.js');
-    const { createCliTransformer } = await import('../lib/transformers/cli.js');
-
-    const transformer = createCliTransformer({
-      color: true,
+    const logger = new Logger({
+      transformer: createCliTransformer({ color: true }),
+      destination: process.stdout,
+      level: Level.Trace,
     });
 
-    const [logger, callback, errback] = createLoggerWithWaitableMock({
-      transformer,
-    });
+    // @ts-ignore
+    const callback = jest.spyOn(console._stdout, 'write').mockImplementation();
 
+    logger.fatal(new Error('herp'));
     logger.error(new Error('hallo'));
     logger.info(new Error('hello'));
     logger.warn(new Error('halo'));
     logger.debug(new Error('gday'));
     logger.trace(new Error('nihao3'));
 
-    await expect(callback.waitUntilCalledTimes(5)).resolves.toMatchSnapshot();
-    expect(errback).not.toBeCalled();
+    await logger.flush();
+
+    expect(callback).toHaveBeenCalledTimes(6);
+    expect(callback.mock.calls).toMatchSnapshot();
+
+    callback.mockRestore();
+  });
+
+  test('force-color off', async () => {
+    const { Level, Logger } = await import('../lib/logger.js');
+    const { createCliTransformer } = await import('../lib/transformers/cli.js');
+
+    const logger = new Logger({
+      transformer: createCliTransformer({ color: false }),
+      destination: process.stdout,
+      level: Level.Trace,
+    });
+
+    // @ts-ignore
+    const callback = jest.spyOn(console._stdout, 'write').mockImplementation();
+
+    logger.fatal(new Error('herp'));
+    logger.error(new Error('hallo'));
+    logger.info(new Error('hello'));
+    logger.warn(new Error('halo'));
+    logger.debug(new Error('gday'));
+    logger.trace(new Error('nihao3'));
+
+    await logger.flush();
+
+    expect(callback).toHaveBeenCalledTimes(6);
+    expect(callback.mock.calls).toMatchSnapshot();
+
+    callback.mockRestore();
   });
 });
